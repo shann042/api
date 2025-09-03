f7cb4ebc4f25a20031986ef11d90704830851ce2
Shanmuga <shanmuga@softdevels.com>
2025-09-03 08:07:12 +0530

    chore and feature:logging

--- config/settings/base.py
+++ config/settings/base.py
@@ -52,7 +52,14 @@ THIRD_PARTY_APPS=[
     "cloudinary",
     "django_celery_beat",
 ]
-LOCAL_APPS=[]
+LOCAL_APPS=[
+    "core_apps.issues",
+    "core_apps.users",
+    "core_apps.common",
+    "core_apps.profiles",
+    "core_apps.ratings",
+    "core_apps.posts",
+]
 INSTALLED_APPS=DJANGO_APPS+THIRD_PARTY_APPS+LOCAL_APPS
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -90,8 +97,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': getenv("POSTGRES_DB"),
+        'USER':getenv("POSTGRES_USER"),
+        'PASSWORD':getenv("POSTGRES_PASSWORD"),
+        'HOST':getenv("POSTGRES_HOST"),
+        'PORT':getenv("POSTGRES_PORT")
     }
 }
 PASSWORD_HASHERS = [
